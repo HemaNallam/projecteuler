b19a64f2c110669a4f31b63e44a35b93ef705e16
HemaNallam <143944162+HemaNallam@users.noreply.github.com>
2024-06-11 10:44:05 +0530

    create 6.py

--- 6.py
+++ 6.py
@@ -1,3 +1,13 @@
-//sum square difference
-n = 100
-print ("Difference: (Σn)² - Σn² =", n*(n-1)*(n+1)*(3*n+2)//12)
+#Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+sum=0
+for i in range(101):
+ sum=sum+i
+sum_square=sum*sum
+print("square of sum of 100 numbers:",sum_square)
+
+temp=0
+for i in range(101):
+  i=i*i
+  temp=temp+i
+print("sum of square of 100 numbers:",temp)
+print("differencebetween the sum of the squares of the first one hundred natural numbers and the square of the sum is ",sum_square-temp)
